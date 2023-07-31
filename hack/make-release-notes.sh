17b48b2413ec6414598d5d2be5addd4b766870c9
Danil Grigorev <danil.grigorev@suse.com>
2023-07-31 09:50:13 +0200

    Integrate release notes file into workflow
    Signed-off-by: Danil Grigorev <danil.grigorev@suse.com>

--- hack/make-release-notes.sh
+++ hack/make-release-notes.sh
@@ -6,9 +6,12 @@ set -o pipefail
 
 TOOLS_FOLDER=${1:-hack/tools/bin}
 
-git clone -n https://github.com/kubernetes-sigs/cluster-api.git --depth 1
-cd cluster-api
-git checkout a0f2568 -- hack/tools/release/notes.go
+mkdir $TOOLS_FOLDER/cluster-api
+cd $TOOLS_FOLDER/cluster-api
+git init
+git remote add origin https://github.com/kubernetes-sigs/cluster-api.git 
+git fetch --depth 1 origin ce045ad2cddf21738799fcc310e847709b8ee41e
+git checkout FETCH_HEAD -- hack/tools/release/notes.go
 go build hack/tools/release/notes.go 
 mv notes $TOOLS_FOLDER
-cd .. && rm -rf cluster-api
+cd - && rm -rf $TOOLS_FOLDER/cluster-api
