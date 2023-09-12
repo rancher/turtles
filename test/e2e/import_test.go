Danil-Grigorev/rancher-turtles

Update e2e tests to use structured rancher types
Signed-off-by: Danil Grigorev <danil.grigorev@suse.com>

test/e2e/import_test.go
@@ -20,29 +20,27 @@ limitations under the License.
 package e2e
 
 import (
-	"fmt"
 	"path/filepath"
 	"strconv"
 
 	"github.com/drone/envsubst/v2"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/rancher-sandbox/rancher-turtles/internal/rancher"
+	provisioningv1 "github.com/rancher-sandbox/rancher-turtles/internal/rancher/provisioning/v1"
 	"sigs.k8s.io/cluster-api/controllers/remote"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	. "sigs.k8s.io/controller-runtime/pkg/envtest/komega"
 
+	turtelesnaming "github.com/rancher-sandbox/rancher-turtles/util/naming"
 	corev1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 )
 
 var _ = Describe("Create and delete CAPI cluster functionality should work", func() {
 	var (
-		rancherClusterHandler *rancher.ClusterHandler
-		rancherCluster        *rancher.Cluster
-		capiCluster           *clusterv1.Cluster
-		rancherClusterKey     client.ObjectKey
+		rancherCluster *provisioningv1.Cluster
+		capiCluster    *clusterv1.Cluster
 		// Manually bumping fleet generation is required to force resources rollout
 		// after deleting them in the cluster by other means then fleet.
 		//
@@ -51,19 +49,16 @@ var _ = Describe("Create and delete CAPI cluster functionality should work", fun
 	)
 
 	BeforeEach(func() {
-		rancherClusterHandler = rancher.NewClusterHandler(ctx, bootstrapClusterProxy.GetClient())
+		SetClient(bootstrapClusterProxy.GetClient())
+		SetContext(ctx)
 		capiCluster = &clusterv1.Cluster{ObjectMeta: metav1.ObjectMeta{
 			Namespace: capiClusterNamespace,
 			Name:      capiClusterName,
 		}}
-		rancherCluster = &rancher.Cluster{ObjectMeta: metav1.ObjectMeta{
+		rancherCluster = &provisioningv1.Cluster{ObjectMeta: metav1.ObjectMeta{
 			Namespace: capiClusterNamespace,
-			Name:      fmt.Sprintf("%s-capi", capiClusterName),
+			Name:      turtelesnaming.Name(capiCluster.Name).ToRancherName(),
 		}}
-		rancherClusterKey = client.ObjectKey{
-			Namespace: rancherCluster.Namespace,
-			Name:      rancherCluster.Name,
-		}
 
 		By("Creating a CAPI cluster with calico CNI")
 		Eventually(func() error {
@@ -76,9 +71,7 @@ var _ = Describe("Create and delete CAPI cluster functionality should work", fun
 		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-controllers")...).Should(Succeed())
 
 		By("Waiting for the CAPI cluster to appear")
-		Eventually(func() error {
-			return bootstrapClusterProxy.GetClient().Get(ctx, client.ObjectKeyFromObject(capiCluster), capiCluster)
-		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(Succeed())
+		Eventually(Get(capiCluster), e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(Succeed())
 	})
 
 	AfterEach(func() {
@@ -86,15 +79,12 @@ var _ = Describe("Create and delete CAPI cluster functionality should work", fun
 		bootstrapClusterProxy.CollectWorkloadClusterLogs(ctx, capiCluster.Namespace, capiCluster.Name, filepath.Join(artifactFolder, "clusters", capiCluster.Name, strconv.Itoa(fleetGeneration)))
 
 		By("Removing CAPI cluster record")
-		Eventually(func() bool {
-			return apierrors.IsNotFound(bootstrapClusterProxy.GetClient().Delete(ctx, capiCluster))
-		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-controllers")...).Should(BeTrue())
+		Eventually(func() error {
+			return bootstrapClusterProxy.GetClient().Delete(ctx, capiCluster)
+		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-controllers")...).Should(Succeed())
 
 		By("Waiting for the rancher cluster record to be removed")
-		Eventually(func() bool {
-			_, err := rancherClusterHandler.Get(rancherClusterKey)
-			return apierrors.IsNotFound(err)
-		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-controllers")...).Should(BeTrue())
+		Eventually(Get(rancherCluster), e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-controllers")...).Should(MatchError(ContainSubstring("not found")))
 	})
 
 	It("should successfully create a rancher cluster from a CAPI cluster", func() {
@@ -109,41 +99,26 @@ var _ = Describe("Create and delete CAPI cluster functionality should work", fun
 		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(Succeed())
 
 		By("Waiting for the rancher cluster record to appear")
-		Eventually(func() error {
-			_, err := rancherClusterHandler.Get(rancherClusterKey)
-			return err
-		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(Succeed())
+		Eventually(Get(rancherCluster), e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(Succeed())
 
 		By("Waiting for the rancher cluster to have a deployed agent")
-		Eventually(func() bool {
-			cluster, err := rancherClusterHandler.Get(rancherClusterKey)
-			return err == nil && cluster.Status.AgentDeployed == true
-		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(BeTrue())
+		Eventually(Object(rancherCluster), e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(HaveField("Status.AgentDeployed", BeTrue()))
 
 		By("Waiting for the rancher cluster to be ready")
-		Eventually(func() bool {
-			cluster, err := rancherClusterHandler.Get(rancherClusterKey)
-			return err == nil && cluster.Status.Ready == true
-		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(BeTrue())
+		Eventually(Object(rancherCluster), e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(HaveField("Status.Ready", BeTrue()))
 	})
 
 	It("should successfully remove a rancher cluster and check it if is no longer re-imported", func() {
 		By("Waiting for the rancher cluster record to appear")
-		Eventually(func() error {
-			_, err := rancherClusterHandler.Get(rancherClusterKey)
-			return err
-		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(Succeed())
+		Eventually(Get(rancherCluster), e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(Succeed())
 
 		By("Removing rancher cluster record, and waiting for it to be removed")
-		Eventually(func() bool {
-			err := rancherClusterHandler.Delete(rancherCluster)
-			return apierrors.IsNotFound(err)
-		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-controllers")...).Should(BeTrue())
+		Eventually(func() error {
+			return bootstrapClusterProxy.GetClient().Delete(ctx, rancherCluster)
+		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-controllers")...).Should(Succeed())
+		Eventually(Get(rancherCluster), e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-controllers")...).Should(MatchError(ContainSubstring("not found")))
 
 		By("Checking if rancher cluster record will not be re-created again")
-		Consistently(func() bool {
-			_, err := rancherClusterHandler.Get(rancherClusterKey)
-			return apierrors.IsNotFound(err)
-		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-consistently")...).Should(BeTrue())
+		Consistently(Get(rancherCluster), e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-consistently")...).Should(MatchError(ContainSubstring("not found")))
 	})
 })

rancher/turtles

test/e2e/import_test.go
@@ -20,28 +20,25 @@ limitations under the License.
 package e2e
 
 import (
-	"fmt"
 	"path/filepath"
 	"strconv"
 
 	"github.com/drone/envsubst/v2"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/rancher-sandbox/rancher-turtles/internal/rancher"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	provisioningv1 "github.com/rancher-sandbox/rancher-turtles/internal/rancher/provisioning/v1"
+	. "sigs.k8s.io/controller-runtime/pkg/envtest/komega"
 
+	turtlesnaming "github.com/rancher-sandbox/rancher-turtles/util/naming"
 	corev1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 )
 
 var _ = Describe("Create and delete CAPI cluster functionality should work", func() {
 	var (
-		rancherClusterHandler *rancher.ClusterHandler
-		rancherCluster        *rancher.Cluster
-		capiCluster           *clusterv1.Cluster
-		rancherClusterKey     client.ObjectKey
+		rancherCluster *provisioningv1.Cluster
+		capiCluster    *clusterv1.Cluster
 		// Manually bumping fleet generation is required to force resources rollout
 		// after deleting them in the cluster by other means then fleet.
 		//
@@ -50,19 +47,16 @@ var _ = Describe("Create and delete CAPI cluster functionality should work", fun
 	)
 
 	BeforeEach(func() {
-		rancherClusterHandler = rancher.NewClusterHandler(ctx, bootstrapClusterProxy.GetClient())
+		SetClient(bootstrapClusterProxy.GetClient())
+		SetContext(ctx)
 		capiCluster = &clusterv1.Cluster{ObjectMeta: metav1.ObjectMeta{
 			Namespace: capiClusterNamespace,
 			Name:      capiClusterName,
 		}}
-		rancherCluster = &rancher.Cluster{ObjectMeta: metav1.ObjectMeta{
+		rancherCluster = &provisioningv1.Cluster{ObjectMeta: metav1.ObjectMeta{
 			Namespace: capiClusterNamespace,
-			Name:      fmt.Sprintf("%s-capi", capiClusterName),
+			Name:      turtlesnaming.Name(capiCluster.Name).ToRancherName(),
 		}}
-		rancherClusterKey = client.ObjectKey{
-			Namespace: rancherCluster.Namespace,
-			Name:      rancherCluster.Name,
-		}
 
 		By("Creating a CAPI cluster with calico CNI")
 		Eventually(func() error {
@@ -75,9 +69,7 @@ var _ = Describe("Create and delete CAPI cluster functionality should work", fun
 		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-controllers")...).Should(Succeed())
 
 		By("Waiting for the CAPI cluster to appear")
-		Eventually(func() error {
-			return bootstrapClusterProxy.GetClient().Get(ctx, client.ObjectKeyFromObject(capiCluster), capiCluster)
-		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(Succeed())
+		Eventually(Get(capiCluster), e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(Succeed())
 	})
 
 	AfterEach(func() {
@@ -85,15 +77,12 @@ var _ = Describe("Create and delete CAPI cluster functionality should work", fun
 		bootstrapClusterProxy.CollectWorkloadClusterLogs(ctx, capiCluster.Namespace, capiCluster.Name, filepath.Join(artifactFolder, "clusters", capiCluster.Name, strconv.Itoa(fleetGeneration)))
 
 		By("Removing CAPI cluster record")
-		Eventually(func() bool {
-			return apierrors.IsNotFound(bootstrapClusterProxy.GetClient().Delete(ctx, capiCluster))
-		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-controllers")...).Should(BeTrue())
+		Eventually(func() error {
+			return bootstrapClusterProxy.GetClient().Delete(ctx, capiCluster)
+		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-controllers")...).Should(Succeed())
 
 		By("Waiting for the rancher cluster record to be removed")
-		Eventually(func() bool {
-			_, err := rancherClusterHandler.Get(rancherClusterKey)
-			return apierrors.IsNotFound(err)
-		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-controllers")...).Should(BeTrue())
+		Eventually(Get(rancherCluster), e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-controllers")...).Should(MatchError(ContainSubstring("not found")))
 	})
 
 	It("should successfully create a rancher cluster from a CAPI cluster", func() {
@@ -105,41 +94,26 @@ var _ = Describe("Create and delete CAPI cluster functionality should work", fun
 		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(Succeed())
 
 		By("Waiting for the rancher cluster record to appear")
-		Eventually(func() error {
-			_, err := rancherClusterHandler.Get(rancherClusterKey)
-			return err
-		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(Succeed())
+		Eventually(Get(rancherCluster), e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(Succeed())
 
 		By("Waiting for the rancher cluster to have a deployed agent")
-		Eventually(func() bool {
-			cluster, err := rancherClusterHandler.Get(rancherClusterKey)
-			return err == nil && cluster.Status.AgentDeployed == true
-		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(BeTrue())
+		Eventually(Object(rancherCluster), e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(HaveField("Status.AgentDeployed", BeTrue()))
 
 		By("Waiting for the rancher cluster to be ready")
-		Eventually(func() bool {
-			cluster, err := rancherClusterHandler.Get(rancherClusterKey)
-			return err == nil && cluster.Status.Ready == true
-		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(BeTrue())
+		Eventually(Object(rancherCluster), e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(HaveField("Status.Ready", BeTrue()))
 	})
 
 	It("should successfully remove a rancher cluster and check it if is no longer re-imported", func() {
 		By("Waiting for the rancher cluster record to appear")
-		Eventually(func() error {
-			_, err := rancherClusterHandler.Get(rancherClusterKey)
-			return err
-		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(Succeed())
+		Eventually(Get(rancherCluster), e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-rancher")...).Should(Succeed())
 
 		By("Removing rancher cluster record, and waiting for it to be removed")
-		Eventually(func() bool {
-			err := rancherClusterHandler.Delete(rancherCluster)
-			return apierrors.IsNotFound(err)
-		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-controllers")...).Should(BeTrue())
+		Eventually(func() error {
+			return bootstrapClusterProxy.GetClient().Delete(ctx, rancherCluster)
+		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-controllers")...).Should(Succeed())
+		Eventually(Get(rancherCluster), e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-controllers")...).Should(MatchError(ContainSubstring("not found")))
 
 		By("Checking if rancher cluster record will not be re-created again")
-		Consistently(func() bool {
-			_, err := rancherClusterHandler.Get(rancherClusterKey)
-			return apierrors.IsNotFound(err)
-		}, e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-consistently")...).Should(BeTrue())
+		Consistently(Get(rancherCluster), e2eConfig.GetIntervals(bootstrapClusterProxy.GetName(), "wait-consistently")...).Should(MatchError(ContainSubstring("not found")))
 	})
 })