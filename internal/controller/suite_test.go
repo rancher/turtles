c6f7648da512ba8950aab9df8cc1933ea6b8dc1a
Furkat Gofurov <furkat.gofurov@suse.com>
2024-01-05 17:38:08 +0200

    Align license header years with 'reference' copyright
    It also adds license headers to few missing go files, Dockerfile
    and Makefile
    
    Signed-off-by: Furkat Gofurov <furkat.gofurov@suse.com>

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright SUSE 2023.
+Copyright © 2023 - 2024 SUSE LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
