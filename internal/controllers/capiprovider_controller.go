ff5d37a180e34d61d73836ed260cef6387332913
Danil-Grigorev <danil.grigorev@suse.com>
2024-08-30 10:38:20 +0200

    Skip capiprovider reconciliation on deletion timestamp
    Signed-off-by: Danil-Grigorev <danil.grigorev@suse.com>

--- internal/controllers/capiprovider_controller.go
+++ internal/controllers/capiprovider_controller.go
@@ -21,12 +21,11 @@ import (
 	"context"
 
 	corev1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	operatorv1 "sigs.k8s.io/cluster-api-operator/api/v1alpha2"
 
@@ -46,19 +45,13 @@ type CAPIProviderReconciler struct {
 //+kubebuilder:rbac:groups=operator.cluster.x-k8s.io,resources=*,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile reconciles the CAPIProvider object.
-func (r *CAPIProviderReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+func (r *CAPIProviderReconciler) Reconcile(ctx context.Context, capiProvider *turtlesv1.CAPIProvider) (ctrl.Result, error) {
 	log := log.FromContext(ctx)
 
-	capiProvider := &turtlesv1.CAPIProvider{ObjectMeta: metav1.ObjectMeta{
-		Name:      req.Name,
-		Namespace: req.Namespace,
-	}}
-	if err := r.Client.Get(ctx, req.NamespacedName, capiProvider); apierrors.IsNotFound(err) {
-		return ctrl.Result{}, nil
-	} else if err != nil {
-		log.Error(err, "Unable to get CAPIProvider manifest: "+req.String())
+	if !capiProvider.DeletionTimestamp.IsZero() {
+		log.Info("Provider is in the process of deletion, skipping reconcile...")
 
-		return ctrl.Result{}, err
+		return ctrl.Result{}, nil
 	}
 
 	return r.reconcileNormal(ctx, capiProvider)
@@ -113,5 +106,5 @@ func (r *CAPIProviderReconciler) SetupWithManager(_ context.Context, mgr ctrl.Ma
 		b = b.Owns(resource)
 	}
 
-	return b.Complete(r)
+	return b.Complete(reconcile.AsReconciler(r.Client, r))
 }
