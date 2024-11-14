d4ec4fab6d185c5c7af2474ab1c4d2ea107c1770
Alexandr Demicev <alexandr.demicev@suse.com>
2024-11-14 18:00:47 +0100

    Configure helm chart for etcd snapshot restore manifests
    Signed-off-by: Alexandr Demicev <alexandr.demicev@suse.com>

--- scripts/process-exp-etcdrestore-manifests.sh
+++ scripts/process-exp-etcdrestore-manifests.sh
@@ -18,3 +18,14 @@ echo "$start_content" | cat - "$filename" > temp && mv temp "$filename"
 # Append content at the end of the file
 echo "$end_content" >> "$filename"
 
+sed -i '' "s/rancher-turtles-system/{{ index .Values \"rancherTurtles\" \"namespace\" }}/g" "$filename"
+sed -i '' "s/rancher-turtles-system/{{ index .Values \"rancherTurtles\" \"features\" \"etcd-snapshot-restore\" \"image\" }}/g" "$filename"
+sed -i '' "/image: ghcr.io\/rancher\/turtles-etcd-snapshot-restore:dev/c\\
+        {{- \$imageVersion := index .Values \"rancherTurtles\" \"features\" \"etcd-snapshot-restore\" \"imageVersion\" -}}\\
+        {{- if contains \"sha256:\" \$imageVersion }}\\
+        image: {{ index .Values \"rancherTurtles\" \"features\" \"etcd-snapshot-restore\" \"image\" }}@{{ index .Values \"rancherTurtles\" \"features\" \"etcd-snapshot-restore\" \"imageVersion\" }}\\
+        {{- else }}\\
+        image: {{ index .Values \"rancherTurtles\" \"features\" \"etcd-snapshot-restore\" \"image\" }}:{{ index .Values \"rancherTurtles\" \"features\" \"etcd-snapshot-restore\" \"imageVersion\" }}\\
+        {{- end }}\\
+" "$filename"
+sed -i '' "s/imagePullPolicy: IfNotPresent/imagePullPolicy: '{{ index .Values \"rancherTurtles\" \"features\" \"etcd-snapshot-restore\" \"imagePullPolicy\" }}'/g" "$filename"
