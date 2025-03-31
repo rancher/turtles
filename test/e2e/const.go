2e648fffe5098a16cf9102405e93804e13adbbf1
Michal Jura <mjura@suse.com>
2025-03-31 09:28:28 +0200

    Enable Fleet bundles for Docker in e2e
    Adding example with ClusterClass for Docker with Kubeadm and RKEv2
    and Fleet bundle to install CNI
    
    Issue: https://github.com/rancher/turtles/issues/1212
    Issue: https://github.com/rancher/turtles/issues/1211

--- test/e2e/const.go
+++ test/e2e/const.go
@@ -87,8 +87,14 @@ var (
 	//go:embed data/cluster-templates/docker-kubeadm.yaml
 	CAPIDockerKubeadm []byte
 
+	//go:embed data/cluster-templates/docker-kubeadm-topology.yaml
+	CAPIDockerKubeadmTopology []byte
+
 	//go:embed data/cluster-templates/docker-rke2.yaml
 	CAPIDockerRKE2 []byte
+
+	//go:embed data/cluster-templates/docker-rke2-topology.yaml
+	CAPIDockerRKE2Topology []byte
 
 	//go:embed data/cluster-templates/aws-eks-mmp.yaml
 	CAPIAwsEKSMMP []byte
